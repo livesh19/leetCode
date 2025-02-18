11d759c6a5d81ad5471d79d4e0a34baaed782f2c
livesh19 <livesh1901@gmail.com>
2025-02-18 23:19:36 +0530

    LC179

---
+++ LC179.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+      String[] numStrings = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            numStrings[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(numStrings, (a, b) -> (b + a).compareTo(a + b));
+
+    
+        if (numStrings[0].equals("0")) {
+            return "0";
+        }
+
+     
+        StringBuilder result = new StringBuilder();
+        for (String num : numStrings) {
+            result.append(num);
+        }
+
+        return result.toString();  
+    }
+}
