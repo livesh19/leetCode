95a661c98e8fd57a0746598686f95c5669c7a6f8
livesh19 <livesh1901@gmail.com>
2025-04-02 20:53:04 +0530

    LC55

---
+++ LC55.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+      int maxy=0;
+      for(int i=0;i<nums.length;i++){
+        if(i>maxy)return false;
+        maxy = Math.max(maxy,i+nums[i]);
+        if(maxy>=nums.length-1)return true;
+      }
+      return false;
+    }
+}
