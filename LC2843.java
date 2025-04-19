5c3bb39df48708dbcd5843d2bfcd0b3a780475fa
livesh19 <livesh1901@gmail.com>
2025-04-19 23:01:37 +0530

    LC2843

---
+++ LC2843.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countSymmetricIntegers(int low, int high) {
+         int count = 0;
+
+        for (int i = low; i <= high; i++) {
+            String numStr = String.valueOf(i);
+            int len = numStr.length();
+
+            
+            if (len % 2 != 0) continue;
+
+            int mid = len / 2;
+            int sum1 = 0, sum2 = 0;
+
+            for (int j = 0; j < mid; j++) {
+                sum1 += numStr.charAt(j) - '0';
+            }
+
+            for (int j = mid; j < len; j++) {
+                sum2 += numStr.charAt(j) - '0';
+            }
+
+            if (sum1 == sum2) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
