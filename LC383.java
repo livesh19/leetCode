8014cca8799ec8ed7da1fded4c7f547c32079c49
livesh19 <livesh1901@gmail.com>
2025-05-07 22:54:25 +0530

    LC383

---
+++ LC383.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> magazineMap = new HashMap<>();
+
+      
+        for (char c : magazine.toCharArray()) {
+            magazineMap.put(c, magazineMap.getOrDefault(c, 0) + 1);
+        }
+
+       
+        for (char c : ransomNote.toCharArray()) {
+            if (!magazineMap.containsKey(c) || magazineMap.get(c) == 0) {
+                return false;
+            }
+            magazineMap.put(c, magazineMap.get(c) - 1); 
+        }
+
+        return true;
+    }
+}
