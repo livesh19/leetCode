ab2ac2b3ba8603124238fd9bfcf70de8d7f91192
livesh19 <livesh1901@gmail.com>
2025-05-09 23:40:09 +0530

    LC1920

---
+++ LC1920.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int newarr []=new int[nums.length];
+        for(int i =0;i<nums.length;i++){
+            newarr[i]=nums[nums[i]];
+        }
+        return newarr;
+    }
+}
