b43b05c0562012fa3f38315e44eb1577e511d265
livesh19 <livesh1901@gmail.com>
2025-05-22 22:12:33 +0530

    update

--- nextGreaterele.java
+++ nextGreaterele.java
@@ -3,10 +3,10 @@ import java.util.Stack;
 
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> map = new HashMap<>(); // Store next greater elements
-        Stack<Integer> stk = new Stack<>(); // Monotonic decreasing stack
+        HashMap<Integer, Integer> map = new HashMap<>(); 
+        Stack<Integer> stk = new Stack<>(); 
 
-        // Traverse nums2 to determine next greater elements
+       
         for (int num : nums2) {
             while (!stk.isEmpty() && stk.peek() < num) {
                 map.put(stk.pop(), num);
@@ -14,12 +14,12 @@ class Solution {
             stk.push(num);
         }
 
-        // Remaining elements in stack have no greater element
+       
         while (!stk.isEmpty()) {
             map.put(stk.pop(), -1);
         }
 
-        // Build the result array for nums1
+      
         for (int i = 0; i < nums1.length; i++) {
             nums1[i] = map.getOrDefault(nums1[i], -1);
         }
