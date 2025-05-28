a1e398cc3672fe0ac0dfeef10689d9e1fc294469
livesh19 <livesh1901@gmail.com>
2025-05-28 22:19:32 +0530

    trianglepattern

---
+++ trianglepattern.java
@@ -0,0 +1,19 @@
+public class trianglepattern {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = n; i > 0; i--) {
+            int x = i - 1;
+            for (int j = 0; j < x; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (n - i) + 1; k++) {
+                System.out.print("*");
+            }
+            for (int k = 0; k < (n - i) + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
