9dfcf45bd347a0bf98337fb74656e9dd899ae0de
livesh19 <livesh1901@gmail.com>
2025-06-25 11:26:35 +0530

    pattern2

---
+++ pattern2.java
@@ -0,0 +1,20 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int n =6;
+       int count =1;
+       int flag =1;
+       for(int i =0;i<n;i++){
+           count = flag;
+           for(int j =0;j<n;j++){
+               count=count>n?count-n:count;
+               System.out.print(count + " ");
+               count++;
+           }
+           flag++;
+           System.out.println(" ");
+       }
+    }
+}
