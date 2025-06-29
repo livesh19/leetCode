6fa4bcd27180fedb3fbe2e83b2787c914028489d
livesh19 <livesh1901@gmail.com>
2025-06-29 23:52:48 +0530

    update

--- rotateArray.java
+++ rotateArray.java
@@ -1,12 +1,12 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         k = k % nums.length;   
-        // divide and conquer method 
+       
         reverse(nums, 0, nums.length - 1); 
         reverse(nums, 0, k - 1); 
         reverse(nums, k, nums.length - 1);  
     }
-    // reverse array function
+   
     private void reverse(int[] nums, int start, int end) {
         while (start < end) {
             int temp = nums[start];
